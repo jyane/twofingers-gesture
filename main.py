0868e4ddec3bef587dc30a0ed1ffc125931676cb
Shohei Kamimori <jyane.kstm@gmail.com>
2022-05-24 18:06:48 -0700

    minor bug fixes

--- main.py
+++ main.py
@@ -12,25 +12,25 @@ def parse_twofingers_action(line):
         if vert.strip() == 'vert' and v_value.strip() == '0.00/0.0':
             l, r = h_value.split('/')
             if (float(l) > 5): # The action was like forward
-                subprocess.Popen(shlex.split('xdotool key alt+Left'), stdout=subprocess.PIPE)
+                subprocess.Popen(shlex.split('xdotool key alt+Left'))
                 return True
             if (float(l) < -5): # The action like backward
-                subprocess.Popen(shlex.split('xdotool key alt+Right'), stdout=subprocess.PIPE)
+                subprocess.Popen(shlex.split('xdotool key alt+Right'))
                 return True
     return False
 
 def main():
     command = 'stdbuf -oL -- libinput debug-events'
     cmd = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
-    ignore = 10
-    fired = False
+    ignore = 0
     for line in cmd.stdout:
-        # skip if an action was fired.
-        if ignore >= 0:
+        # skip 10 lines if an action was fired.
+        if 0 < ignore:
             ignore = ignore - 1
             continue
-        ignore = 10
         fired = parse_twofingers_action(line)
+        if fired:
+            ignore = 10
 
 if __name__ == '__main__':
     main()
